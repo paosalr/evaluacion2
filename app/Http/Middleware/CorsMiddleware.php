56d33f9557582ae3cdbebd27b1afce07dc649019
paolasalazar <paola.s.satoritech@gmail.com>
2025-03-31 13:16:32 -0600

    Archivo CorsMiddleware

---
+++ app/Http/Middleware/CorsMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CorsMiddleware
+{
+    public function handle(Request $request, Closure $next)
+    {
+        $allowedOrigin = 'http://localhost:5173';
+
+        $headers = [
+            'Access-Control-Allow-Origin' => $allowedOrigin,
+            'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers' => 'Content-Type, Authorization',
+        ];
+
+        if ($request->isMethod('OPTIONS')) {
+            return response()->json(['status' => 'OK'], 200, $headers);
+        }
+
+        $response = $next($request);
+        foreach ($headers as $key => $value) {
+            $response->header($key, $value);
+        }
+
+        return $response;
+    }
+}
