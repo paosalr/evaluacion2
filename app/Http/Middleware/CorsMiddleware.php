f978e6f6479957d29cd8cdd7b9d65ec9d6eb3c2d
paolasalazar <paola.s.satoritech@gmail.com>
2025-03-31 13:37:48 -0600

    CorsMiddleware changes

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -14,7 +14,7 @@ class CorsMiddleware
         $headers = [
             'Access-Control-Allow-Origin' => $allowedOrigin,
             'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS',
-            'Access-Control-Allow-Headers' => 'Content-Type, Authorization',
+            'Access-Control-Allow-Headers' => 'Content-Type, Authorization, Accept, X-Requested-With',
         ];
 
         if ($request->isMethod('OPTIONS')) {
